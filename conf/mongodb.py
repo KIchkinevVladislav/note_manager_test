f2dc20d9b0fcfa98efd93d791a065c9438661baa
vladislav <v.kichkinev@microline.ru>
2025-02-19 18:33:19 +0300

    Fix: local mongo connection

--- conf/mongodb.py
+++ conf/mongodb.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 @environ.config(prefix='MONGO')
 class MongoDBConfig:
-    host: str = environ.var(default='mongo')
+    host: str = environ.var(default='localhost')
     port: str = environ.var(default='27017')
     database: str = environ.var()
 
