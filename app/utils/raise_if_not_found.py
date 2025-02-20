9a93f2653623bdb0dedf7ef71130ddb1e77a924f
vladislav <v.kichkinev@microline.ru>
2025-02-20 22:37:45 +0300

    Ref: change style

--- app/utils/raise_if_not_found.py
+++ app/utils/raise_if_not_found.py
@@ -1,9 +1,10 @@
 from functools import wraps
+from typing import Callable
 
 from app.crud.exceptions import NoteNotFoundException
 
 
-def raise_if_not_found(func):
+def raise_if_not_found(func: Callable):
         @wraps(func)
         def wrapper(*args, **kwargs):
             note = func(*args, **kwargs)
